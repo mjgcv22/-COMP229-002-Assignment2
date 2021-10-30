32707084e3b9d2356f4689dd526eb52d55ae96c6
mjgcv22 <mjgvinarao@gmail.com>
2021-10-30 13:53:07 -0400

    Complete Auth/MongoDB Atlas

---
+++ server/config/db.ts
@@ -0,0 +1,4 @@
+//export const LocalURI = "mongodb://localhost:27017/portfolio";
+export const RemoteURI = "mongodb+srv://mjgcv22:Electronics2014@cluster0.szde2.mongodb.net/portfolio?retryWrites=true&w=majority";
+export const HostName = "localhost";
+export const Secret = "NotSoSecret";
